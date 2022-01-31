f677c8cac9079dfd85afa5cd505bf3584ba71519
soni-z <97900417+soni-z@users.noreply.github.com>
2022-01-31 17:11:56 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -19,33 +19,26 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
-         Scanner sc=new Scanner(System.in);
+      
         Card[] magicHand = new Card[7];
-         Random crd = new Random();
-        Card c1=new Card();
+  
+      
 //            Card game = new Card();
             for (int i = 0; i < magicHand.length; i++) {
 
                 // step 1: generate 7 random cards and store in array
                 Card c = new Card();
-                int value = crd.nextInt(13 - 1) + 1;
-                int crdSuit = crd.nextInt(4);
+            
 
                 c.setValue(value);//random generation for number/use method
 //          c.setSuit("suit by random suit generation"+Card.SUITS[crdSuit]);
                 magicHand[i] = c; // stores the card object  in array
                 c.setSuit(Card.SUITS[crdSuit]);
-                System.out.println("Random cards generated are : "+c.toString());
-                System.out.println("");
+                System.out.println("Random cards generated are : ");
+      
                
 //                System.out.println(" The suit is " + c.getSuit() + " and card number is " + c.getValue() + "\n");
             }
-             System.out.println("Enter card number");
-        int value = sc.nextInt();
-                System.out.println("Enter the suit");
-            String  suit = sc.nextLine();   
-   
-           System.out.println(c1.getLuckyCard());
     
 }
 }
